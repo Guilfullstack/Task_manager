62aacd2eaaed345beca39d2e29aab9c5ec949fc1
Guilherme Almeida   (_|*=*|_) <117943472+Guilfullstack@users.noreply.github.com>
2023-04-19 09:26:31 -0300

    Create main.c
    Criação dos menus inicial e principal

---
+++ main.c
@@ -0,0 +1,28 @@
+/***
+Para entender como funciona cada recurso
+Acesse esse link= https://replit.com/@GuilhermeAlme18/testes
+***/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    /***MENU INICIAL***/
+    printf("BEM VINDO!\nDIGITE UMA OPCAO!\
+           \n1- LOGAR \
+           \n2- CADASTRAR \
+            \n3- SAIR \
+            \n>>");
+    /***MENU PRINCIPAL***/
+    printf("\nO QUE DESEJA ACESSAR? \
+           \n1- PERFIL \
+            \n2- ENXAMES \
+            \n3- ESPPECIES \
+            \n4- TAREFAS \
+            \n5- PRODUCAO \
+            \n6- RELATORIOS \
+            \n7- ENCERRAR \
+            \n>>");
+
+
+    return 0;
+}
